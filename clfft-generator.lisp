45e101b48e9b9dcec002afdb1185e37e23d9568b
Martin Kielhorn <kielhorn.martin@gmail.com>
2015-09-06 11:21:05 +0200

    move doc into readme

--- clfft-generator.lisp
+++ clfft-generator.lisp
@@ -1,16 +1,3 @@
-;; This program generates a C99 source which in turn uses clFFT to
-;; calculate the 2D FFT of a real array. The absolute value of the
-;; transform result is plotted using Gnuplot.
-
-;; Required software to convert this program into the C99 source
-;; clfft.c: Common Lisp (I use SBCL), C-Mera (I installed it in
-;; ~/quicklisp/local-projects and load it using Quicklisp).
-
-;; Required software to run the generated program: make, gcc, some
-;; OpenCL ICD (I developed using the one from Intel), clFFT (the
-;; OpenCL initialization code is based on their example in the
-;; README.md), gnuplot (for realtime plotting)
-
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
